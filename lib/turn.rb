d5bd67feff7bd33cca00d2600860e267f7b88dc8
Christiana Vandermale <christianavdm@gmail.com>
2018-04-06 00:23:10 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,3 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(number)
+  number.to_i - 1
+end
+
+def move (board, position, xo = "X")
+  board[position] = xo
+end
