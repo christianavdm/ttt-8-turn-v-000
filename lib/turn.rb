8e5fe52bef8decc8b5e4e1e71c551079ed2ffbf1
Christiana Vandermale <christianavdm@gmail.com>
2018-04-06 00:24:35 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,3 +14,18 @@ def move (board, position, xo = "X")
   board[position] = xo
 end
 
+def valid_move?(board, index)
+  if index.between?(0, 8) && position_taken?(board, index) == false
+    return true
+  else 
+    return false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
