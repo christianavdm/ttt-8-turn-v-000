a6e24044bd41433bdf9e9d33b3d539f007d78244
Christiana Vandermale <christianavdm@gmail.com>
2018-04-06 01:04:08 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index) == false
     turn(board)
-  elsif valid_move?(board, index) == true
+  else valid_move?(board, index) == true
     move(board, index, xo = "X")
     display_board(board)
   end
